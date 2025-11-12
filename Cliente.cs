c2570a4a0be9006f99915b04b49976f7d51b50e8
andreslan123 <andressanticla@gmail.com>
2025-11-12 15:47:31 -0400

    Primera carga de proyecto de github

---
+++ Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GESTION_TIENDA_VIRTUALwpfPROGRAMII
+{
+    class Cliente
+    {
+        //PROPIEDADES 
+        public int IdCli { get; set; }
+        public string Nombre { get; set; }
+        public List<Producto> lstProdVta { get; set; }
+
+        //CONSTRUCTORES
+        public Cliente()
+        {
+            IdCli = 0;
+            Nombre = "no definido";
+        }
+        public Cliente(int idcli,string nomcli)
+        {
+            IdCli = idcli;
+            Nombre = nomcli;
+        }
+    }
+}
---
+++ Producto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GESTION_TIENDA_VIRTUALwpfPROGRAMII
+{
+    class Producto
+    {
+        //PROPIEDADES 
+        public int IdProd { get; set; }
+        public string NombreProd { get; set; }
+        public double PrecioProd { get; set; }
+        //CONSTRUCTORES
+        public Producto()
+        {
+            IdProd = 0;
+            NombreProd = "No definido";
+            PrecioProd = 0;
+        }
+        public Producto(int idProd, string NombrePro, double Precio)
+        {
+            IdProd = idProd;
+            NombreProd = NombrePro;
+            PrecioProd = Precio;
+        }
+    }
+}
